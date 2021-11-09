599f8d62c18e5c948c8d01be5109a520b7319ef2
Gabriel Bernier <gabrielbernier2@gmail.com>
2021-11-09 15:51:03 -0500

    Objets plats dans la liste

--- TP214E/Data/DAL.cs
+++ TP214E/Data/DAL.cs
@@ -65,7 +65,7 @@ namespace TP214E.Data
             try
             {
                 IMongoDatabase db = mongoDBClient.GetDatabase("TP2DB");
-                plats = db.GetCollection<Plat>("plats");
+                plats = db.GetCollection<Plat>("Plats");
             }
             catch (Exception ex)
             {
