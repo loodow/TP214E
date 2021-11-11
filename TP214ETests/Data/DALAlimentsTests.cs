3073d5bbedfd839074844db6b9577198867c15e1
Ludovic Poitras <ludovicpoitras@gmail.com>
2021-11-10 20:04:52 -0500

    debut tests DAL Aliments

---
+++ TP214ETests/Data/DALAlimentsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP214E.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP214ETests.Data
+{
+    [TestClass()]
+    public class DALAlimentsTests
+    {
+        [TestMethod()]
+        public void AjouterAlimentTest()
+        {
+            DALAliments dALAliments = new DALAliments();
+        }
+
+        [TestMethod()]
+        public void AlimentsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RetirerAlimentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModifierAlimentTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
