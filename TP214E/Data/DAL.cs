7da01dbaa68a5c0403765d08a3ab0245fab00f5d
Gabriel Bernier <gabrielbernier2@gmail.com>
2021-11-11 01:14:35 -0500

    cleanup DAL

--- TP214E/Data/DAL.cs
+++ TP214E/Data/DAL.cs
@@ -20,44 +20,7 @@ namespace TP214E.Data
             mongoDBClient = OuvrirConnexion();
         }
 
-        //DAL Aliments
-        public IMongoCollection<Aliment> Aliments()
-        {
-            try
-            {
-                IMongoDatabase db = mongoDBClient.GetDatabase("TP2DB");
-                aliments = db.GetCollection<Aliment>("Aliments");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Impossible de se connecter à la base de données " + ex.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            return aliments;
-        }
-
-
-        public async void AjouterAliment(Aliment aliment)
-        {
-            await aliments.InsertOneAsync(aliment);
-        }
-
-        public void RetirerAliment(Aliment aliment)
-        {
-            var filtre = Builders<Aliment>.Filter.Eq("_id", aliment._id);
-            aliments.DeleteOne(filtre);
-        }
-
-        public void ModifierAliment(Aliment aliment, ObjectId idAliment)
-        {
-            var filtre = Builders<Aliment>.Filter.Eq("_id", idAliment);
-            var update = Builders<Aliment>.Update
-                .Set("Nom", aliment.Nom)
-                .Set("Quantite", aliment.Quantite)
-                .Set("ExpireLe", aliment.ExpireLe)
-                .Set("Unite", aliment.Unite);
-            aliments.UpdateOne(filtre, update);
-        }
-
+        
         //DAL Plats 
 
 
