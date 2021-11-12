45fbf4200c19b923bbbdecccb5a87ace3b17725c
Ludovic Poitras <ludovicpoitras@gmail.com>
2021-11-12 11:33:20 -0500

    ObtenirChaineFormateePlatsEtTotal + accesseur

--- TP214E/Data/Commande.cs
+++ TP214E/Data/Commande.cs
@@ -14,5 +14,41 @@ namespace TP214E.Data
         public ObjectId _id { get; set; }
         public List<Plat> PlatsCommande { get; set; }
 
+        public string ChaineFormateePlatsEtTotal
+        {
+            get { return ObtenirChaineFormateePlatsEtTotal(); }
+        }
+
+        public string ObtenirChaineFormateePlatsEtTotal()
+        {
+            if (PlatsCommande.Count == 1)
+            {
+                return $"{PlatsCommande[0].Nom} ({CalculerTotalPrixPlats()}$)";
+            }
+            else if (PlatsCommande.Count == 2)
+            {
+                return $"{PlatsCommande[0].Nom}, {PlatsCommande[1].Nom} ({CalculerTotalPrixPlats()}$)";
+            }
+            else if (PlatsCommande.Count == 3)
+            {
+                return $"{PlatsCommande[0].Nom}, {PlatsCommande[1].Nom} + 1 autre ({CalculerTotalPrixPlats()}$)";
+            }
+            else if (PlatsCommande.Count > 3)
+            {
+                return $"{PlatsCommande[0].Nom}, {PlatsCommande[1].Nom} + {PlatsCommande.Count - 2} autres ({CalculerTotalPrixPlats()}$)";
+            }
+
+            return "Aucun plats dans la commande";
+        }
+
+        private double CalculerTotalPrixPlats()
+        {
+            double total = 0;
+
+            foreach (Plat plat in PlatsCommande)
+                total += plat.Prix;
+
+            return Math.Round(total, 2);
+        }
     }
 }
