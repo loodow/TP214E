e79dd99916e4e0051b7c74c833d25bc327279f78
Gabriel Bernier <gabrielbernier2@gmail.com>
2021-11-13 20:43:37 -0500

    tests nom aliment

---
+++ TP214ETests/Data/AlimentTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP214E.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+using MongoDB.Driver;
+using Assert = NUnit.Framework.Assert;
+
+namespace TP214ETests.Data
+{
+
+    [TestFixture]
+    public class AlimentTests
+    {
+        [Test]
+        public void TestGetSetNom()
+        {
+
+            Aliment aliment = new Aliment();
+            string nom = "toast";
+
+            aliment.Nom = nom;
+
+            Assert.AreEqual(aliment.Nom, nom);
+
+
+        }
+
+
+
+    }
+}
